35242c188f72bcc17057c7348f8c2f4598d1232b
Buğra Coşkun <bugracoskun1996@gmail.com>
2021-04-22 19:37:24 +0300

    corrections

--- get_trips_data.py
+++ get_trips_data.py
@@ -16,17 +16,15 @@ port=f.readline().rstrip("\n")
 p = postgres(database, user,password,host,port)
 
 #INPUTS
-stage=3 # yolculuğun kaçancı parçası alınacak
+stage=7 # yolculuğun kaçancı parçası alınacak
 default_mmsi=219005068
-start_datee=datetime(2020,12,1, 00, 00, 00, 00)
-start_epoch=math.ceil(datetime(2020,12,1, 00, 00, 00, 00).timestamp())
 table_name="ships"
-time_range=timedelta(minutes=10*stage)
+time_range=timedelta(minutes=10)
 
 # ANALYSE
 f2 = open("trajectory_ships_20min_219005068_local.txt", "r")
 
-with open('219005068_10min_3.csv', mode='w') as csv_file:
+with open('219005068_10min_7.csv', mode='w') as csv_file:
     points_fieldnames = ['time', 'lat', 'lon']
     writer = csv.DictWriter(csv_file, fieldnames=points_fieldnames)
     writer.writeheader()
@@ -40,12 +38,11 @@ with open('219005068_10min_3.csv', mode='w') as csv_file:
             datee=datee.split("-")
             start_time=line[3].split(":")
             start_time2=line[2]+" "+line[3]
-            general_start_time_obj=datetime(int(datee[0]),int(datee[1]),int(datee[2]),int(start_time[0]),int(start_time[1]),int(start_time[2]))
 
             finish_time=line[5].split(":")
             finish_time2=line[4]+" "+line[5]
             finish_epoch=datetime(int(datee[0]),int(datee[1]),int(datee[2]),int(finish_time[0]),int(finish_time[1]),int(finish_time[2]))
-            epoch1=datetime(int(datee[0]),int(datee[1]),int(datee[2]),int(start_time[0]),int(start_time[1]),int(start_time[2]))
+            epoch1=datetime(int(datee[0]),int(datee[1]),int(datee[2]),int(start_time[0]),int(start_time[1]),int(start_time[2]))+(time_range*stage)
             epoch2=epoch1+time_range
 
             # get points 
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from datetime import timedelta
 deneme={
     "day1":[(34,45),(2,2)],
     "day2":[(35,35),(3,3)]
@@ -11,3 +13,5 @@ if("day4" in deneme):
     print(deneme["day4"])
 else:
     print("yok")
+
+print(timedelta(minutes=10)*3)
