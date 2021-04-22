4b7b5189503a757a83a045e4beb416e39f66158c
Buğra Coşkun <bugracoskun1996@gmail.com>
2021-04-22 20:05:55 +0300

    stages

--- get_trips_data.py
+++ get_trips_data.py
@@ -16,7 +16,7 @@ port=f.readline().rstrip("\n")
 p = postgres(database, user,password,host,port)
 
 #INPUTS
-stage=3 # yolculuğun kaçancı parçası alınacak
+stage=7 # yolculuğun kaçancı parçası alınacak
 default_mmsi=219005068
 table_name="ships"
 time_range=timedelta(minutes=10)
@@ -24,7 +24,7 @@ time_range=timedelta(minutes=10)
 # ANALYSE
 f2 = open("trajectory_ships_20min_219005068_local.txt", "r")
 
-with open('219005068_10min_3.csv', mode='w') as csv_file:
+with open('219005068_10min_'+str(stage)+'.csv', mode='w') as csv_file:
     points_fieldnames = ['time', 'lat', 'lon']
     writer = csv.DictWriter(csv_file, fieldnames=points_fieldnames)
     writer.writeheader()
@@ -42,9 +42,11 @@ with open('219005068_10min_3.csv', mode='w') as csv_file:
             finish_time=line[5].split(":")
             finish_time2=line[4]+" "+line[5]
             finish_epoch=datetime(int(datee[0]),int(datee[1]),int(datee[2]),int(finish_time[0]),int(finish_time[1]),int(finish_time[2]))
-            epoch1=datetime(int(datee[0]),int(datee[1]),int(datee[2]),int(start_time[0]),int(start_time[1]),int(start_time[2]))+(time_range*stage)
+            epoch1=datetime(int(datee[0]),int(datee[1]),int(datee[2]),int(start_time[0]),int(start_time[1]),int(start_time[2]))+(time_range*(stage-1))
             epoch2=epoch1+time_range
 
+            print(epoch1)
+            print(epoch2)
             # get points 
             points_info=p.getVesselSpecificTimewithTimeInfo(table_name,mmsi,epoch1,epoch2)
             for poi in points_info:
